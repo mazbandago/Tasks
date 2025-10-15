a063209e8c93e6f71fb8c20ca566f4a8eaa3c64d
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-15 20:54:07 +0000

    feat:refactor the navbar and dashboardNav to be consistent in design

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 
 function DashboardNav() {
   return (
-    <nav className="bg-indigo-700 shadow-md p-4">
+    <nav className="bg-indigo-700 shadow-md p-4 rounded-t-2xl">
        <div className="max-w-6xl mx-auto flex justify-between items-cener">
-            <Link to="/"className="text-white text-2xl font-bold hover:text-indigo-200 transition duration-150 mr-8 ">Home</Link>
-            <Link to="/dashboard"className="text-white text-2xl font-bold hover:text-indigo-200 transition duration-150 ">Football news</Link>
-            <Link to="/dashboard/politics"className="text-white text-2xl font-bold hover:text-indigo-200 transition duration-150 ">Political news</Link>
+            <Link to="/"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Home</Link>
+            <Link to="/dashboard"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Football News</Link>
+            <Link to="/dashboard/politics"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Political News</Link>
         </div> 
     </nav>
   )
