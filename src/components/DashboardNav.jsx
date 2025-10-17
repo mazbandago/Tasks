c14ff3c3b893ade2942997bb6cca3cfbb0ecec2f
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-17 22:16:36 +0000

    refactor the nav bars to reduce font size

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -12,10 +12,10 @@ function DashboardNav() {
   return (
     <nav className="bg-indigo-700 shadow-md p-4 rounded-t-2xl">
        <div className="max-w-6xl mx-auto flex justify-between items-cener">
-            <Link to="/"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Home</Link>
-            <Link to="/dashboard"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Football News</Link>
-            <Link to="/dashboard/politics"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Political News</Link>
-            <button onClick={handleLogout} className="text-white text-2xl font-bold border px-2 py-2 bg-red-600 rounded hover:bg-red-800 shadow-xl transition duration-150">Logout</button>
+            <Link to="/"className="text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Home</Link>
+            <Link to="/dashboard"className="text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Football News</Link>
+            <Link to="/dashboard/politics"className="text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Political News</Link>
+            <button onClick={handleLogout} className="text-white text-sm font-bold border px-0.5 py-0.5 bg-red-600 rounded hover:bg-red-800 shadow-xl transition duration-150">Logout</button>
         </div> 
     </nav>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ function Navbar() {
   return (
         <nav className="bg-indigo-700 shadow-md p-4 rounded-t-2xl">
           <div className="max-w-6xl mx-auto flex justify-between items-cener">
-            <Link to="/" className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150 ">Home</Link>
-            <Link to="/generalnews" className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150 ">General News</Link>
-            <Link to="/signinform" className='text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150'>Login</Link>
-            <Link to="/dashboard" className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150 ">Dashboard</Link>
+            <Link to="/" className="text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150 ">Home</Link>
+            <Link to="/generalnews" className="text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150 ">General News</Link>
+            <Link to="/signinform" className='text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150'>Login</Link>
+            <Link to="/dashboard" className="text-white text-sm font-bold border px-0.5 py-0.5 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150 ">Dashboard</Link>
           </div>
         </nav>
   )
