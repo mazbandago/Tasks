0796aecdc65e5e1cc91ddbb7a8319c1626facaa8
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-17 21:56:10 +0000

    refactor to add login button to dashboard nav

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 function DashboardNav() {
+  const Navigate = useNavigate();
+  const handleLogout = () =>{
+  localStorage.removeItem("isLoggedIn");
+  Navigate("/signinform")
+  }
+
   return (
     <nav className="bg-indigo-700 shadow-md p-4 rounded-t-2xl">
        <div className="max-w-6xl mx-auto flex justify-between items-cener">
             <Link to="/"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Home</Link>
             <Link to="/dashboard"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Football News</Link>
             <Link to="/dashboard/politics"className="text-white text-2xl font-bold border px-2 py-2 bg-black rounded hover:bg-blue-600 shadow-xl transition duration-150">Political News</Link>
+            <button onClick={handleLogout} className="text-white text-2xl font-bold border px-2 py-2 bg-red-600 rounded hover:bg-red-800 shadow-xl transition duration-150">Logout</button>
         </div> 
     </nav>
   )
