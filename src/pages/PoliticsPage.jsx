0876c1028a43bb2439db55ee01b9c022a4ba7fe8
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-17 22:43:50 +0000

    remove one of the dashboard nav

--- src/pages/PoliticsPage.jsx
+++ src/pages/PoliticsPage.jsx
@@ -4,7 +4,6 @@ import DashboardNav from '../components/DashboardNav';
 function PoliticsPage() {
   return (
     <div>
-      <DashboardNav />
       <h1>This is the Politics Page</h1>
     </div>
   )
