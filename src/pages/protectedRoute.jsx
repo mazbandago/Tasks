30b0407ba120980adb187024dbf4cfc85ef5e4f7
Mustapha Abdulai <mazbandago@gmail.com>
2025-10-17 17:31:09 +0000

    refactor the protected rout page to enable navigation to the protected page when logged in

--- src/pages/protectedRoute.jsx
+++ src/pages/protectedRoute.jsx
@@ -1,9 +1,10 @@
-import { Navigate, Outlet } from "react-router-dom";
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectedRoute = () => {
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+function protectedRoute() {
+    const isLoggedIn = localStorage.getItem("isLoggedIn")==="true";
+    return isLoggedIn? <Outlet /> : <Navigate to="/signinform"/>
 
-  return isLoggedIn ? <Outlet /> : <Navigate to="/signinform" />;
-};
+}
 
-export default ProtectedRoute;
+export default protectedRoute;
